cc07a2affc49e12ac2d7136fd623b483fa47aed5
g21turbo <dg21stew@gmail.com>
2023-11-09 22:08:19 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static int getLargestPrime(int number) {
+
+        if (number < 0){
+            return -1;
+        }
+
+    }
+
+}
